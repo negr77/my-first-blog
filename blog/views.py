e1c625be6c85b0fc50d0a52d2658e0c8623249e4
Zelim <gaysultanov20@gmail.com>
2024-08-04 11:20:08 +0300

    Mysite version 3. Add file blog/urls.pt

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 def index(request):
     return render(request, "blog/index.html", {})\
     
-def categ(request):
-    return HttpResponse('<h1>page1</h1>')
+def categ(request, catid):
+    return HttpResponse(f'<h1>page1 {catid}</h1>')
